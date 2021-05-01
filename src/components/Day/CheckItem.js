930a5f8cef3b3e1c3d70a51bc35e86068ccea996
chicichich <pch6789@naver.com>
2021-05-01 23:56:50 +0900

    Add: CheckItem 컴포넌트

---
+++ src/components/Day/CheckItem.js
@@ -0,0 +1,51 @@
+const checkItem = {
+  constructor: function (target) {
+    this.item = document.createElement("div");
+    target.appendChild(this.item);
+    this.checkbox = document.createElement("input");
+    this.checkbox.type = "checkbox";
+    this.checkbox.addEventListener("input", this.checkboxInputEvent.bind(this));
+    this.item.appendChild(this.checkbox);
+    this.itemLabel = document.createElement("span");
+    this.item.appendChild(this.itemLabel);
+    this.label = document.createElement("label");
+    this.label.hidden = true;
+    this.label.addEventListener("click", this.labelClickEvent.bind(this));
+    this.input = document.createElement("input");
+    this.input.addEventListener("change", this.inputChangeEvent.bind(this));
+    this.itemLabel.appendChild(this.label);
+    this.itemLabel.appendChild(this.input);
+  },
+  setLabel: function (label) {
+    this.label.innerText = label;
+  },
+  setLabelStrikethrough: function (status) {
+    if (status) {
+      this.label.style.textDecoration = "line-through";
+    } else {
+      this.label.style.textDecoration = "none";
+    }
+  },
+  getChecked: function () {
+    return this.checkbox.checked;
+  },
+  checkboxInputEvent: function (e) {
+    this.setLabelStrikethrough(e.target.checked);
+  },
+  labelClickEvent: function (e) {
+    this.label.hidden = true;
+    this.input.hidden = false;
+  },
+  inputChangeEvent: function (e) {
+    if (e.target.value) {
+      this.input.hidden = true;
+      this.setLabel(e.target.value);
+      this.label.hidden = false;
+    }
+  },
+  destructor: function () {
+    this.item.remove();
+  },
+};
+
+export default checkItem;
