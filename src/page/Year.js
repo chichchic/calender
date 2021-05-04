9538c9e7804b2bafc655ba7de2a528b70f392702
chicichich <pch6789@naver.com>
2021-05-04 16:47:40 +0900

    Update: 연도 변경 기능

--- src/page/Year.js
+++ src/page/Year.js
@@ -3,22 +3,47 @@ import { getCalender } from "../fixture/caleder";
 
 const Year = {
   constructor: function () {
-    const year = 2021;
     this.view = document.createElement("section");
     this.view.className = "year-view";
-    const yearTitle = document.createElement("h3");
-    yearTitle.className = "year-title";
-    yearTitle.textContent = year;
-    this.view.appendChild(yearTitle);
-    const yearCalender = getCalender(year);
+    const controller = document.createElement("div");
+    controller.className = "controller";
+    this.view.appendChild(controller);
+    this.yearTitle = document.createElement("h3");
+    this.yearTitle.className = "year-title";
+    const prevButton = document.createElement("button");
+    prevButton.addEventListener("click", (e) => {
+      this._render(this.year - 1);
+    });
+    prevButton.innerHTML = "&minus;";
+    const nextButton = document.createElement("button");
+    nextButton.addEventListener("click", (e) => {
+      this._render(this.year + 1);
+    });
+    nextButton.innerHTML = "&plus;";
+    controller.appendChild(this.yearTitle);
+    controller.appendChild(prevButton);
+    controller.appendChild(nextButton);
+    this.calenderView = document.createElement("article");
+    this.calenderView.className = "calender-view";
+    this.view.appendChild(this.calenderView);
+    this._render(2021);
+    return this.view;
+  },
+  _render: function (year) {
+    this.year = year;
+    this._makeCalenderView(this.calenderView);
+    this.yearTitle.innerText = this.year;
+  },
+  _makeCalenderView: function () {
+    const yearCalender = getCalender(this.year);
+    this.calenderView.innerHTML = "";
     for (const month in yearCalender) {
       const MonthInstance = Object.create(MonthCalender).constructor({
         month,
         weekInfo: yearCalender[month],
       });
-      this.view.appendChild(MonthInstance);
+      this.calenderView.appendChild(MonthInstance);
     }
-    return this.view;
   },
 };
 
